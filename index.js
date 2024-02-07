cb3e40080724e24dea5e571921bd82de17554982
rebebop <1941785+rebebop@users.noreply.github.com>
2024-02-07 13:28:56 +0500

    Initial lift off

---
+++ index.js
@@ -0,0 +1,31 @@
+const core = require("@actions/core");
+
+async function main() {
+  try {
+    const appCenterToken = core.getInput("appcenter-token");
+    const projectName = encodeURIComponent(core.getInput("project-name"));
+    const sourceBranch = core.getInput("source-branch");
+    const targetBranch = core.getInput("target-branch");
+
+    const appCenterUrl = `https://api.appcenter.ms/v0.1/apps/${projectName}/branches/${targetBranch}/config`;
+
+    const options = {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        "x-api-token": appCenterToken,
+      },
+      body: JSON.stringify({
+        cloneFromBranch: sourceBranch,
+      }),
+    };
+
+    const response = await fetch(appCenterUrl, options);
+
+    console.log(response);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+main();
