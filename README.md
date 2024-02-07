eedc4467a6af389a2f297658f37a1a14e3590d7c
rebebop <1941785+rebebop@users.noreply.github.com>
2024-02-07 15:50:23 +0500

    Add starting the build if the flag is set

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
-# Cloning a configuration from AppCenter to another branch
+# Setup AppCenter build
 
 This action clones a build configuration
 from a source branch to a target branch in AppCenter.
+It can also optionally start a new build.
 
-You can use this to setup an automatic build configuration
-on each pull request as shown in the example.
+If a build configuration already exists it will not create a new one.
 
 ## Inputs
 
-### `appcenter-token`
+### `appcenter-api-token`
 
 **Required** "AppCenter API token, you can generate it from your profile page". https://docs.microsoft.com/en-us/appcenter/api-docs/#creating-an-app-center-app-api-token.
 
@@ -16,21 +16,26 @@ on each pull request as shown in the example.
 
 **Required** "Name of project e.g: org/project_name"
 
-### `source-branch`
-
-**Required** "Name of the branch to be cloned" (cloneFromBranch) (https://openapi.appcenter.ms/#/build/branchConfigurations_create).
-
 ### `target-branch`
 
 **Required** "Name of the target branch on which the configuration is applied"
 
+### `source-branch`
+
+"Name of the branch to be cloned" (cloneFromBranch) (https://openapi.appcenter.ms/#/build/branchConfigurations_create).
+
+### `start-build`
+
+Whether or not the action should start the build as well. ("yes"/"no")
+
 ## Example usage
 
 ```
-uses: rebebop/appcenter-clone-configuration@1.0
+uses: rebebop/appcenter-clone-configuration@main
 with:
   appcenter-token: '${{ secrets.APP_CENTER_TOKEN }}'
   source-branch: 'master'
   target-branch: 'feature/exciting-feature'
   project-name: 'rebebop/excitingApp-ios'
+  start-build: "yes"
 ```
--- dist/index.js
+++ dist/index.js
@@ -24741,19 +24741,40 @@ async function main() {
         const projectName = core.getInput("project-name");
         const sourceBranch = core.getInput("source-branch");
         const targetBranch = encodeURIComponent(core.getInput("target-branch"));
-        const appCenterUrl = `https://api.appcenter.ms/v0.1/apps/${projectName}/branches/${targetBranch}/config`;
-        const options = {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "X-API-Token": appCenterToken,
-            },
-            body: JSON.stringify({
-                cloneFromBranch: sourceBranch,
-            }),
+        const startBuild = core.getInput("start-build");
+        const headers = {
+            "Content-Type": "application/json",
+            "X-API-Token": appCenterToken,
         };
-        const response = await fetch(appCenterUrl, options);
-        console.log(response);
+        const appCenterConfUrl = `https://api.appcenter.ms/v0.1/apps/${projectName}/branches/${targetBranch}/config`;
+        // Check whether the configuration already exists
+        const configExists = await fetch(appCenterConfUrl, {
+            method: "GET",
+            headers,
+        });
+        if (configExists.ok) {
+            console.log("Configuration for this branch already exists on AppCenter");
+        }
+        else {
+            // Only create the configuration if one does not already exists
+            const cloneConfiguration = await fetch(appCenterConfUrl, {
+                headers,
+                method: "POST",
+                body: JSON.stringify({
+                    cloneFromBranch: sourceBranch,
+                }),
+            });
+            console.log(cloneConfiguration);
+        }
+        // Start the build if it is enabled
+        if (startBuild === "yes") {
+            const appCenterBuildUrl = `https://api.appcenter.ms/v0.1/apps/${projectName}/branches/${targetBranch}/builds`;
+            const startBuild = await fetch(appCenterBuildUrl, {
+                headers,
+                method: "POST",
+            });
+            console.log(startBuild);
+        }
     }
     catch (error) {
         console.log(error);
