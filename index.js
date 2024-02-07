58d9637f18a6e8cda379e221e94b11d36a733dc2
rebebop <1941785+rebebop@users.noreply.github.com>
2024-02-07 13:52:26 +0500

    Fix encoding on branch name

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const core = require("@actions/core");
 async function main() {
   try {
     const appCenterToken = core.getInput("appcenter-token");
-    const projectName = encodeURIComponent(core.getInput("project-name"));
+    const projectName = core.getInput("project-name");
     const sourceBranch = core.getInput("source-branch");
-    const targetBranch = core.getInput("target-branch");
+    const targetBranch = encodeURIComponent(core.getInput("target-branch"));
 
     const appCenterUrl = `https://api.appcenter.ms/v0.1/apps/${projectName}/branches/${targetBranch}/config`;
 
